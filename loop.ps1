0946dde521722500c996dbab37b28d56f9a19939
Raphael <113527833+Phillybb@users.noreply.github.com>
2023-07-13 14:21:43 +0200

    simple loop
    Simple loading screen to let your users now a process is happening

---
+++ loop.ps1
@@ -0,0 +1,11 @@
+cls
+
+Write-Host -NoNewline "."
+Wait-Event -Timeout 1
+Write-Host -NoNewline "."
+Wait-Event -Timeout 1
+Write-Host -NoNewline "."
+Wait-Event -Timeout 1
+Write-Host -NoNewline "."
+Wait-event -Timeout 1
+Write-Host "           "
